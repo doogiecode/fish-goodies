2d04b19820237b958ae2918ea8f6b6c040b2f4b1
William Proffitt <proffitt@datamininglab.com>
2021-10-05 15:50:02 -0400

    Fixed git branch name reporting after going from fish 3.1 to 3.3.
    Each command execution began spewing "fatal: not a git repository (or any of the parent directories): .git".

--- config.fish
+++ config.fish
@@ -17,7 +17,7 @@ function fish_prompt --description 'Write out the prompt'
     end
 	
     function _git_branch_name
-        echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+        echo (git symbolic-ref --quiet HEAD 2>&1 | sed -e '/not a git/d' | sed -e 's|^refs/heads/||')
     end
     function _is_git_dirty
         echo (git status -s --ignore-submodules=dirty ^/dev/null)
